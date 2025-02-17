d3ea298f57e48679db0d3f39455bacee928b8486
Alejandra0714 <alejrsabov07@gmail.com>
2025-02-17 15:33:48 -0600

    Add files via upload

---
+++ practicas/Tarea 2 ejercicio 3/ejercicio.js
@@ -0,0 +1,3 @@
+document.getElementById("cambiarTexto").addEventListener("click", function() {
+    document.getElementById("mensaje").innerHTML = "¡La gente suele cambiar demasiado, tanto que siempre existe algo nuevo para observar en ellos siempre.!";
+});
